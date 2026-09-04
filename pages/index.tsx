7259e292f01a5eeebb57e259faf901aff69a6456
TTAWDTT <2451989@tongji.edu.cn>
2026-09-04 23:45:56 +0800

    fix(copy): add color reply to portrait caption

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,9 @@ export default function HomePage({ documents, tree }: { documents: DocumentMeta[
               <p className="home-page__lead">
                 此博客纯属像我这样的性压抑的产物。
               </p>
-              <p className="home-page__note">当成臆想症就好。</p>
+              <p className="home-page__note">
+                当成臆想症就好。<br />但是你的鼠标移过来我就有色彩了哦！
+              </p>
               <Link className="home-page__link" href="#latest">
                 读最新文章 <span aria-hidden="true">↘</span>
               </Link>
